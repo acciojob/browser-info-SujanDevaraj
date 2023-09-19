0eb608ce9edf28ec99ede25fb046928da0893dae
SujanDevaraj <96370355+SujanDevaraj@users.noreply.github.com>
2023-09-19 13:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+ const browserInfoDiv = document.getElementById('browser-info');
+ browserInfoDiv.textContent = "You are using " + navigator.userAgent + " version " + navigator.appVersion;
